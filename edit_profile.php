22b25a6f7903f7690f630cae10a3a5ae0eb12bff
Mohammad Jadidi <mohammadjadidi2877@gmail.com>
2020-11-06 10:43:27 +0330

    Add files via upload
    Final Project - Send Message / Send Post / Change Info.

---
+++ edit_profile.php
@@ -0,0 +1,452 @@
+<?php 
+session_start();
+include("includes/connection.php");
+include("functions/functions.php");
+
+if(!isset($_SESSION['user_email'])){
+	
+	header("location: index.php"); 
+}
+else {
+?>
+<!DOCTYPE html> 
+<html>
+	<head>
+		<title>Welcome User!</title> 
+	<link rel="stylesheet" href="styles/home_style.css" media="all"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	</head> 
+	
+<body>
+	<!--Container starts--> 
+	<div class="container">
+		<!--Header Wrapper Starts-->
+	
+			<!--Header Starts-->
+			
+								<ul class="menu">
+					<li><a href="home.php">Home</a></li>
+					<li><a href="members.php">Members</a></li>
+					
+					<?php 
+					
+					$get_topics = "select * from topics"; 
+					$run_topics = mysqli_query($con,$get_topics);
+					
+					while($row=mysqli_fetch_array($run_topics)){
+						
+						$topic_id = $row['topic_id'];
+						$topic_title = $row['topic_title'];
+					
+					echo "<li><a href='topic.php?topic=$topic_id'>$topic_title</a></li>";
+					}
+					
+					?>
+										<form method="get" action="results.php" id="form1">
+					<input class="search" name="user_query" type="text" placeholder="Search">
+					</form>
+				</ul>
+	
+			<!--Header ends-->
+	
+		<!--Header Wrapper ends-->
+			<!--Content area starts-->
+			<div class="profile">
+				<!--user timeline starts-->
+				<div id="user_timeline">
+					<div id="user_mention">
+					<?php 
+					$user = $_SESSION['user_email'];
+					$get_user = "select * from users where user_email='$user'"; 
+					$run_user = mysqli_query($con,$get_user);
+					$row=mysqli_fetch_array($run_user);
+					
+					$user_id = $row['user_id']; 
+					$user_name = $row['user_name'];
+					$user_pass = $row['user_pass'];
+					$user_email = $row['user_email'];
+					$user_country = $row['user_country'];
+					$user_gender = $row['user_gender'];
+					$user_image = $row['user_image'];
+					$register_date = $row['register_date'];
+					$last_login = $row['last_login'];
+										$user_posts = "select * from posts where user_id='$user_id'"; 
+					$run_posts = mysqli_query($con,$user_posts); 
+					$posts = mysqli_num_rows($run_posts);
+					//getting the number of unread messages 
+					$sel_msg = "select * from messages where receiver='$user_id' AND status='unread' ORDER by 1 DESC"; 
+					$run_msg = mysqli_query($con,$sel_msg);		
+		
+					$count_msg = mysqli_num_rows($run_msg);
+					
+					echo "
+						<center>
+						<img src='user/user_images/$user_image' width='200' height='200'/>
+						</center><hr>
+						<div>
+						<p><strong>Name:</strong> $user_name</p>
+						<p><strong>Country:</strong> $user_country</p>
+						<p><strong>Last Login:</strong> $last_login</p>
+						<p><strong>Member Since:</strong> $register_date</p>
+						
+						<button><a href='my_messages.php?inbox&u_id=$user_id'>Messages ($count_msg)</a></button>
+						<button><a href='my_posts.php?u_id=$user_id'>My Posts ($posts)</a></button>
+						<button><a href='edit_profile.php?u_id=$user_id'>Edit My Account</a></button>
+						<button><a href='logout.php'>Logout</a></button>
+						</div>
+					";
+					?>
+					</div>
+				</div>
+				</div>
+				<!--user timeline ends-->
+				<!--Content timeline starts-->
+				
+				<div class="content_timeline">
+					<div class="edit">
+					
+					<form action="" method="post" id="f" class="ff" enctype="multipart/form-data">
+					
+					<table>
+						<tr align="center">
+							<td colspan="6"><h2>Edit Your Profile:</h2></td>
+						</tr>
+						<tr>
+							<td align="right">Name:</td>
+							<td>
+							<input type="text" name="u_name" required="required" value="<?php echo $user_name;?>"/>
+							</td>
+						</tr>
+						
+						<tr>
+							<td align="right">Password:</td>
+							<td>
+							<input type="password" name="u_pass" required="required" value="<?php echo $user_pass;?>"/>
+							</td>
+						</tr>
+						
+						<tr>
+							<td align="right">Email:</td>
+							<td>
+							<input type="email" name="u_email" required="required" value="<?php echo $user_email;?>"></td>
+						</tr>
+						
+						<tr>
+							<td align="right">Country:</td>
+							<td>
+							<select name="u_country" disabled="disabled">
+								<option><?php echo $user_country;?></option>
+<option value="Afganistan">Afghanistan</option>
+   <option value="Albania">Albania</option>
+   <option value="Algeria">Algeria</option>
+   <option value="American Samoa">American Samoa</option>
+   <option value="Andorra">Andorra</option>
+   <option value="Angola">Angola</option>
+   <option value="Anguilla">Anguilla</option>
+   <option value="Antigua & Barbuda">Antigua & Barbuda</option>
+   <option value="Argentina">Argentina</option>
+   <option value="Armenia">Armenia</option>
+   <option value="Aruba">Aruba</option>
+   <option value="Australia">Australia</option>
+   <option value="Austria">Austria</option>
+   <option value="Azerbaijan">Azerbaijan</option>
+   <option value="Bahamas">Bahamas</option>
+   <option value="Bahrain">Bahrain</option>
+   <option value="Bangladesh">Bangladesh</option>
+   <option value="Barbados">Barbados</option>
+   <option value="Belarus">Belarus</option>
+   <option value="Belgium">Belgium</option>
+   <option value="Belize">Belize</option>
+   <option value="Benin">Benin</option>
+   <option value="Bermuda">Bermuda</option>
+   <option value="Bhutan">Bhutan</option>
+   <option value="Bolivia">Bolivia</option>
+   <option value="Bonaire">Bonaire</option>
+   <option value="Bosnia & Herzegovina">Bosnia & Herzegovina</option>
+   <option value="Botswana">Botswana</option>
+   <option value="Brazil">Brazil</option>
+   <option value="British Indian Ocean Ter">British Indian Ocean Ter</option>
+   <option value="Brunei">Brunei</option>
+   <option value="Bulgaria">Bulgaria</option>
+   <option value="Burkina Faso">Burkina Faso</option>
+   <option value="Burundi">Burundi</option>
+   <option value="Cambodia">Cambodia</option>
+   <option value="Cameroon">Cameroon</option>
+   <option value="Canada">Canada</option>
+   <option value="Canary Islands">Canary Islands</option>
+   <option value="Cape Verde">Cape Verde</option>
+   <option value="Cayman Islands">Cayman Islands</option>
+   <option value="Central African Republic">Central African Republic</option>
+   <option value="Chad">Chad</option>
+   <option value="Channel Islands">Channel Islands</option>
+   <option value="Chile">Chile</option>
+   <option value="China">China</option>
+   <option value="Christmas Island">Christmas Island</option>
+   <option value="Cocos Island">Cocos Island</option>
+   <option value="Colombia">Colombia</option>
+   <option value="Comoros">Comoros</option>
+   <option value="Congo">Congo</option>
+   <option value="Cook Islands">Cook Islands</option>
+   <option value="Costa Rica">Costa Rica</option>
+   <option value="Cote DIvoire">Cote DIvoire</option>
+   <option value="Croatia">Croatia</option>
+   <option value="Cuba">Cuba</option>
+   <option value="Curaco">Curacao</option>
+   <option value="Cyprus">Cyprus</option>
+   <option value="Czech Republic">Czech Republic</option>
+   <option value="Denmark">Denmark</option>
+   <option value="Djibouti">Djibouti</option>
+   <option value="Dominica">Dominica</option>
+   <option value="Dominican Republic">Dominican Republic</option>
+   <option value="East Timor">East Timor</option>
+   <option value="Ecuador">Ecuador</option>
+   <option value="Egypt">Egypt</option>
+   <option value="El Salvador">El Salvador</option>
+   <option value="Equatorial Guinea">Equatorial Guinea</option>
+   <option value="Eritrea">Eritrea</option>
+   <option value="Estonia">Estonia</option>
+   <option value="Ethiopia">Ethiopia</option>
+   <option value="Falkland Islands">Falkland Islands</option>
+   <option value="Faroe Islands">Faroe Islands</option>
+   <option value="Fiji">Fiji</option>
+   <option value="Finland">Finland</option>
+   <option value="France">France</option>
+   <option value="French Guiana">French Guiana</option>
+   <option value="French Polynesia">French Polynesia</option>
+   <option value="French Southern Ter">French Southern Ter</option>
+   <option value="Gabon">Gabon</option>
+   <option value="Gambia">Gambia</option>
+   <option value="Georgia">Georgia</option>
+   <option value="Germany">Germany</option>
+   <option value="Ghana">Ghana</option>
+   <option value="Gibraltar">Gibraltar</option>
+   <option value="Great Britain">Great Britain</option>
+   <option value="Greece">Greece</option>
+   <option value="Greenland">Greenland</option>
+   <option value="Grenada">Grenada</option>
+   <option value="Guadeloupe">Guadeloupe</option>
+   <option value="Guam">Guam</option>
+   <option value="Guatemala">Guatemala</option>
+   <option value="Guinea">Guinea</option>
+   <option value="Guyana">Guyana</option>
+   <option value="Haiti">Haiti</option>
+   <option value="Hawaii">Hawaii</option>
+   <option value="Honduras">Honduras</option>
+   <option value="Hong Kong">Hong Kong</option>
+   <option value="Hungary">Hungary</option>
+   <option value="Iceland">Iceland</option>
+   <option value="Indonesia">Indonesia</option>
+   <option value="India">India</option>
+   <option value="Iran">Iran</option>
+   <option value="Iraq">Iraq</option>
+   <option value="Ireland">Ireland</option>
+   <option value="Isle of Man">Isle of Man</option>
+   <option value="Israel">Israel</option>
+   <option value="Italy">Italy</option>
+   <option value="Jamaica">Jamaica</option>
+   <option value="Japan">Japan</option>
+   <option value="Jordan">Jordan</option>
+   <option value="Kazakhstan">Kazakhstan</option>
+   <option value="Kenya">Kenya</option>
+   <option value="Kiribati">Kiribati</option>
+   <option value="Korea North">Korea North</option>
+   <option value="Korea Sout">Korea South</option>
+   <option value="Kuwait">Kuwait</option>
+   <option value="Kyrgyzstan">Kyrgyzstan</option>
+   <option value="Laos">Laos</option>
+   <option value="Latvia">Latvia</option>
+   <option value="Lebanon">Lebanon</option>
+   <option value="Lesotho">Lesotho</option>
+   <option value="Liberia">Liberia</option>
+   <option value="Libya">Libya</option>
+   <option value="Liechtenstein">Liechtenstein</option>
+   <option value="Lithuania">Lithuania</option>
+   <option value="Luxembourg">Luxembourg</option>
+   <option value="Macau">Macau</option>
+   <option value="Macedonia">Macedonia</option>
+   <option value="Madagascar">Madagascar</option>
+   <option value="Malaysia">Malaysia</option>
+   <option value="Malawi">Malawi</option>
+   <option value="Maldives">Maldives</option>
+   <option value="Mali">Mali</option>
+   <option value="Malta">Malta</option>
+   <option value="Marshall Islands">Marshall Islands</option>
+   <option value="Martinique">Martinique</option>
+   <option value="Mauritania">Mauritania</option>
+   <option value="Mauritius">Mauritius</option>
+   <option value="Mayotte">Mayotte</option>
+   <option value="Mexico">Mexico</option>
+   <option value="Midway Islands">Midway Islands</option>
+   <option value="Moldova">Moldova</option>
+   <option value="Monaco">Monaco</option>
+   <option value="Mongolia">Mongolia</option>
+   <option value="Montserrat">Montserrat</option>
+   <option value="Morocco">Morocco</option>
+   <option value="Mozambique">Mozambique</option>
+   <option value="Myanmar">Myanmar</option>
+   <option value="Nambia">Nambia</option>
+   <option value="Nauru">Nauru</option>
+   <option value="Nepal">Nepal</option>
+   <option value="Netherland Antilles">Netherland Antilles</option>
+   <option value="Netherlands">Netherlands (Holland, Europe)</option>
+   <option value="Nevis">Nevis</option>
+   <option value="New Caledonia">New Caledonia</option>
+   <option value="New Zealand">New Zealand</option>
+   <option value="Nicaragua">Nicaragua</option>
+   <option value="Niger">Niger</option>
+   <option value="Nigeria">Nigeria</option>
+   <option value="Niue">Niue</option>
+   <option value="Norfolk Island">Norfolk Island</option>
+   <option value="Norway">Norway</option>
+   <option value="Oman">Oman</option>
+   <option value="Pakistan">Pakistan</option>
+   <option value="Palau Island">Palau Island</option>
+   <option value="Palestine">Palestine</option>
+   <option value="Panama">Panama</option>
+   <option value="Papua New Guinea">Papua New Guinea</option>
+   <option value="Paraguay">Paraguay</option>
+   <option value="Peru">Peru</option>
+   <option value="Phillipines">Philippines</option>
+   <option value="Pitcairn Island">Pitcairn Island</option>
+   <option value="Poland">Poland</option>
+   <option value="Portugal">Portugal</option>
+   <option value="Puerto Rico">Puerto Rico</option>
+   <option value="Qatar">Qatar</option>
+   <option value="Republic of Montenegro">Republic of Montenegro</option>
+   <option value="Republic of Serbia">Republic of Serbia</option>
+   <option value="Reunion">Reunion</option>
+   <option value="Romania">Romania</option>
+   <option value="Russia">Russia</option>
+   <option value="Rwanda">Rwanda</option>
+   <option value="St Barthelemy">St Barthelemy</option>
+   <option value="St Eustatius">St Eustatius</option>
+   <option value="St Helena">St Helena</option>
+   <option value="St Kitts-Nevis">St Kitts-Nevis</option>
+   <option value="St Lucia">St Lucia</option>
+   <option value="St Maarten">St Maarten</option>
+   <option value="St Pierre & Miquelon">St Pierre & Miquelon</option>
+   <option value="St Vincent & Grenadines">St Vincent & Grenadines</option>
+   <option value="Saipan">Saipan</option>
+   <option value="Samoa">Samoa</option>
+   <option value="Samoa American">Samoa American</option>
+   <option value="San Marino">San Marino</option>
+   <option value="Sao Tome & Principe">Sao Tome & Principe</option>
+   <option value="Saudi Arabia">Saudi Arabia</option>
+   <option value="Senegal">Senegal</option>
+   <option value="Seychelles">Seychelles</option>
+   <option value="Sierra Leone">Sierra Leone</option>
+   <option value="Singapore">Singapore</option>
+   <option value="Slovakia">Slovakia</option>
+   <option value="Slovenia">Slovenia</option>
+   <option value="Solomon Islands">Solomon Islands</option>
+   <option value="Somalia">Somalia</option>
+   <option value="South Africa">South Africa</option>
+   <option value="Spain">Spain</option>
+   <option value="Sri Lanka">Sri Lanka</option>
+   <option value="Sudan">Sudan</option>
+   <option value="Suriname">Suriname</option>
+   <option value="Swaziland">Swaziland</option>
+   <option value="Sweden">Sweden</option>
+   <option value="Switzerland">Switzerland</option>
+   <option value="Syria">Syria</option>
+   <option value="Tahiti">Tahiti</option>
+   <option value="Taiwan">Taiwan</option>
+   <option value="Tajikistan">Tajikistan</option>
+   <option value="Tanzania">Tanzania</option>
+   <option value="Thailand">Thailand</option>
+   <option value="Togo">Togo</option>
+   <option value="Tokelau">Tokelau</option>
+   <option value="Tonga">Tonga</option>
+   <option value="Trinidad & Tobago">Trinidad & Tobago</option>
+   <option value="Tunisia">Tunisia</option>
+   <option value="Turkey">Turkey</option>
+   <option value="Turkmenistan">Turkmenistan</option>
+   <option value="Turks & Caicos Is">Turks & Caicos Is</option>
+   <option value="Tuvalu">Tuvalu</option>
+   <option value="Uganda">Uganda</option>
+   <option value="United Kingdom">United Kingdom</option>
+   <option value="Ukraine">Ukraine</option>
+   <option value="United Arab Erimates">United Arab Emirates</option>
+   <option value="United States of America">United States of America</option>
+   <option value="Uraguay">Uruguay</option>
+   <option value="Uzbekistan">Uzbekistan</option>
+   <option value="Vanuatu">Vanuatu</option>
+   <option value="Vatican City State">Vatican City State</option>
+   <option value="Venezuela">Venezuela</option>
+   <option value="Vietnam">Vietnam</option>
+   <option value="Virgin Islands (Brit)">Virgin Islands (Brit)</option>
+   <option value="Virgin Islands (USA)">Virgin Islands (USA)</option>
+   <option value="Wake Island">Wake Island</option>
+   <option value="Wallis & Futana Is">Wallis & Futana Is</option>
+   <option value="Yemen">Yemen</option>
+   <option value="Zaire">Zaire</option>
+   <option value="Zambia">Zambia</option>
+   <option value="Zimbabwe">Zimbabwe</option>
+							</select>
+							</td>
+						</tr>
+						
+						<tr>
+							<td align="right" >Gender:</td>
+							<td>
+							<select name="u_gender" disabled="disabled">
+								<option><?php echo $user_gender;?></option>
+								<option>Male</option>
+								<option>Female</option>
+								
+							</select>
+							</td>
+						</tr>
+						
+						<tr>
+							<td align="right">Photo:</td>
+							<td>
+							<input type="file" name="u_image" required="required"/>
+							</td>
+						</tr>
+						
+						<tr align="center">
+							<td colspan="6">
+							<input type="submit" name="update" value="Update"/>
+							</td>
+						</tr>
+					</table>
+				</form>
+<?php 
+	if(isset($_POST['update'])){
+	
+		$u_name = $_POST['u_name']; 
+		$u_pass = $_POST['u_pass'];
+		$u_email = $_POST['u_email'];
+		$u_image = $_FILES['u_image']['name'];
+		$image_tmp = $_FILES['u_image']['tmp_name'];
+		
+		move_uploaded_file($image_tmp,"user/user_images/$u_image");
+		
+		$update = "update users set user_name='$u_name', user_pass='$u_pass',user_email='$u_email',user_image='$u_image' where user_id='$user_id'";
+		
+		$run = mysqli_query($con,$update); 
+		
+		if($run){
+		
+		echo "<script>alert('Your Profile Updated!')</script>";
+		echo "<script>window.open('home.php','_self')</script>";
+		}
+	
+	}
+
+
+?>
+					
+				</div>
+				<!--Content timeline ends-->
+			</div>
+			<!--Content area ends-->
+		</div>
+	
+	<!--Container ends-->
+
+</body>
+</html>
+<?php } ?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+
+include("functions/functions.php");
+include("login.php");
+include("template/header.php");
+
+
+?>
+
+		
+		
+		
+		
